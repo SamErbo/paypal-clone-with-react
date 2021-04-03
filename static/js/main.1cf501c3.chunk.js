b3241141d398ee56222afa4bf471cbae3861784f
SamErbo <samuel.ermias@gmail.com>
2021-04-03 20:47:48 +0300

    Updates

---
+++ static/js/main.1cf501c3.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppaypal-clone-with-react"]=this["webpackJsonppaypal-clone-with-react"]||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);var c=n(1),a=n.n(c),r=n(3),s=n.n(r),i=n(0);var o=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("h1",{children:" PayPal is for everyone who pays online. "})})};s.a.render(Object(i.jsx)(a.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.1cf501c3.chunk.js.map
